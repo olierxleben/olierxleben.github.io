8ed84fd3938bc5fe8d51cc3565a9f5311b9c6365
Oliver Erxleben <oliver.erxleben@me.com>
2019-06-16 12:37:13 +0200

    dev: added clickable navigation

--- gh-page-source/src/app/app-routing.module.ts
+++ gh-page-source/src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
 import { AboutmePageComponent } from './components/aboutme-page/aboutme-page.component';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
 
 const routes: Routes = [
   { path: 'home', component: HomePageComponent },
-  { path: 'blog', component: HomePageComponent },
+  { path: 'blog', component: BlogPageComponent },
   { path: 'about', component: AboutmePageComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: NotFoundPageComponent }
